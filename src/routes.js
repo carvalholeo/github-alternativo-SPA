954f0bec8e51d383d16e44969723291821c13df5
Leo Carvalho <carvalho.csleo@gmail.com>
2021-10-05 11:18:39 -0300

    Instala Axios Cria Pesquisa Cria Config do Axios Cria rota de pesquisa

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Error404 from './pages/Errors/Error404';
 import Disabled from './pages/Errors/Disabled';
+import Pesquisa from './pages/Pesquisa'
 
 function Routes() {
   return (
     <div className="container-lg">
       <Switch>
 
+        <Route path='/pesquisa' component={Pesquisa} />
         <Route path='/contact' component={Disabled} />
         <Route component={Error404} />
       </Switch>
